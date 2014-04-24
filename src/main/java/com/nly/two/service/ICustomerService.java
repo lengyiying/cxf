db87fb4523015514b5c592ea514b00cadf8f6d8c
unknown <078825@SH-078825.deppon.com>
2014-04-24 17:35:18 +0800

    commited by nly at 0424

--- src/main/java/com/nly/two/service/ICustomerService.java
+++ src/main/java/com/nly/two/service/ICustomerService.java
@@ -3,10 +3,21 @@ package com.nly.two.service;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
+import org.apache.cxf.annotations.DataBinding;
+import org.apache.cxf.annotations.WSDLDocumentation;
+import org.apache.cxf.annotations.WSDLDocumentation.Placement;
+import org.apache.cxf.annotations.WSDLDocumentationCollection;
+
 import com.nly.two.exception.NoCustomerException;
 import com.nly.two.java.Customer;
 
 @WebService
+@DataBinding(org.apache.cxf.aegis.databinding.AegisDatabinding.class)
+@WSDLDocumentationCollection({
+	@WSDLDocumentation("My portType documentation"),
+	@WSDLDocumentation(value="My top level documentation",placement=Placement.TOP),
+	@WSDLDocumentation(value="My binding doc",placement=Placement.BINDING)
+})
 public interface ICustomerService {
 	Customer[] getCustomerByName(@WebParam(name="userName")String name) throws NoCustomerException;
 }
